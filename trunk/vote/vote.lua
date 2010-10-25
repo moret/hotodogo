06073f5892652e6f5aaed0e7e7ae30da54eb766b
google@moret.pro.br <google@moret.pro.br@620e11e1-e46b-76f3-8bc6-96e244ec4181>
2010-10-25 17:42:41 +0000

    Mudando tratamento de eventos em Lua para a VM do Telemidia

--- trunk/vote/vote.lua
+++ trunk/vote/vote.lua
@@ -2,6 +2,7 @@ votesOption1 = 0
 votesOption2 = 0
 
 function luaVote(evt)
+	--print("evt.class: " .. evt.class .. "; evt.action: " .. evt.action .. "; evt.type: " .. evt.type .. "; evt.label: " .. evt.label)
 	if evt.class == "ncl" and evt.action == "start" and evt.type == "presentation" then
 		if evt.label == "luaVote1" then
 			votesOption1 = votesOption1 + 1
@@ -11,36 +12,43 @@ function luaVote(evt)
 			votesOption2 = votesOption2 + 1
 		end
 		
-		--print("option1: " .. votesOption1 .. "; option2: " .. votesOption2)
-		
-		local voteCondition = 0; -- tie		
+		-- tie
+		local voteCondition = 0
+		-- option1 landslide		
 		if votesOption1 > votesOption2 * 2 then
-			voteCondition = 1 -- option1 landslide
+			voteCondition = 1
+		-- option1 simple win
 		elseif votesOption1 > votesOption2 then
-			voteCondition = 2 -- option1 simple win
+			voteCondition = 2
+		-- option2 landslide
 		elseif votesOption2 > votesOption1 * 2 then
-			voteCondition = 3 -- option2 landslide
+			voteCondition = 3
+		-- option2 simple win
 		elseif votesOption2 > votesOption1 then
-			voteCondition = 4 -- option2 simple win
+			voteCondition = 4
 		end
 	
 		local evtCondition = {
-			class = "ncl",
-			type = "attribution",
-			name = "luaVoteResult",
+			class = 'ncl',
+			type = 'attribution',
+			name = 'luaVoteResult',
 			value = voteCondition
 		}
 	
-		evtCondition.action = "start"
+		--print("evtCondition.name: " .. evtCondition.name .. "; evtCondition.value: " .. evtCondition.value)			
+	
+		evtCondition.action = 'start'
 		event.post("out", evtCondition)
-		--evtCondition.action = "stop"
-		--event.post("out", evtCondition)
-	
+		evtCondition.action = 'stop'
+		event.post("out", evtCondition)
 	end
-	
-	evt.action = "stop"
-	event.post("out", evt)
+
+	print("option1: " .. votesOption1 .. "; option2: " .. votesOption2)
+	--if evt.name then
+	--	evt.action = "stop"
+	--	event.post("out", evt)
+	--end
 end
 
+--print("event.register(luaVote)")
 event.register(luaVote)
-
